6ccce282421485ec6c6cd3a8cdedc7ab1bbb6efe
rohith7 <danush25vip@gmail.com>
2017-06-10 20:00:32 -0600

    changed hello and program

--- Program.java
+++ Program.java
@@ -6,7 +6,7 @@ public static void main(String[] args){
 for(int i=25;i<30;i++){
 
 
-for(int j=35;j<40;j++){
+for(int j=35;j<50;j++){
 System.out.println("Hello Mr i....."+i);
 System.out.println("Hello Mr j......"+j);
 }
